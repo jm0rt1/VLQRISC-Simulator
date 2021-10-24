29fef46df6488bd3fe8364f497684cf2c48c7e5f
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-24 15:23:53 -0400

    exceptions, added memory size, and conversion function for register names

--- src/VLQRISC_Assembler/hw_definitions.py
+++ src/VLQRISC_Assembler/hw_definitions.py
@@ -1,4 +1,29 @@
 
 
-RegisterNames = ["$zero", "$t0", "$t1",
-                 "$t2", "$t3", "$s0", "$s1", "$s2", "$s3", "$s4", "$a0", "$a1", "$v0"]
+class NotARegister(Exception):
+    pass
+
+
+REGISTER_NAMES = ["$zero",
+                  "$t0",
+                  "$t1",
+                  "$t2",
+                  "$t3",
+                  "$s0",
+                  "$s1",
+                  "$s2",
+                  "$s3",
+                  "$s4",
+                  "$a0",
+                  "$a1",
+                  "$v0"]
+
+MEMORY_SIZE = 2 ** 16  # 2^16 Bytes
+
+
+def convert_reg_common_name_to_number(common_name: str):
+    for i, name in enumerate(REGISTER_NAMES):
+        if name == common_name:
+            return i
+    else:
+        raise NotARegister(f"'{common_name}' is not a valid register")
