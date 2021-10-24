8d5c53566fd3545f63faa5f6f3feb5c531724c8a
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-24 15:24:51 -0400

    testing hw_definitions

--- tests/VLQRISC_Assembler/test_hw_definitions.py
+++ tests/VLQRISC_Assembler/test_hw_definitions.py
@@ -4,7 +4,7 @@ import src.VLQRISC_Assembler.hw_definitions as hw_definitions
 
 class TestHWDefinitions(unittest.TestCase):
     def test_hw_definitions(self):
-        length = len(hw_definitions.RegisterNames)
+        length = len(hw_definitions.REGISTER_NAMES)
         if length >= 16:
             raise Exception(
                 f"Can only support 16 Registers with design -- you have {length}")
