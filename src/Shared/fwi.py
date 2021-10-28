4f05bd75d2694116392aa3b43d9f50e285bc414f
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-27 22:44:35 -0400

    Fixed width integer added

--- src/Shared/fwi.py
+++ src/Shared/fwi.py
@@ -1,5 +1,5 @@
 # fwi = Fixed Width Integers
-from src.Shared.utils import signed_bin_str_to_int, int_to_sign_extended_bin_str
+from src.Shared.utils import convert_int_bin_str, int_to_sign_extended_bin_str, signed_bin_str_to_int, unsigned_bin_str_to_int
 
 
 class FWIOverFlow(Exception):
@@ -13,16 +13,37 @@ class FWI():
         super().__init__()
         self._lower = -2**(width-1)
         self._upper = (2**(width-1))-1
-        if self._lower <= int <= self._upper:
+        if not(self._lower <= int <= self._upper):
             raise FWIOverFlow(
                 "Fixed int passed in with fixed width integer exceeds limits")
         self.width = width
         self.int = int
 
+    @property
     def bits(self) -> str:
         return int_to_sign_extended_bin_str(self.int, self.width)
 
+    @classmethod
+    def from_binary_str(cls, binary_string: str):
+
+        return cls(signed_bin_str_to_int(binary_string), len(binary_string))
+
 
 class FWI_unsigned():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, int: int, width: int = 16) -> None:
+        super().__init__()
+        self._lower = 0
+        self._upper = (2**(width))-1
+        if not(self._lower <= int <= self._upper):
+            raise FWIOverFlow(
+                "Fixed int passed in with fixed width integer exceeds limits")
+        self.width = width
+        self.int = int
+
+    @property
+    def bits(self) -> str:
+        return convert_int_bin_str(self.int, self.width)
+
+    @classmethod
+    def from_binary_str(cls, binary_string: str):
+        return cls(unsigned_bin_str_to_int(binary_string), len(binary_string))
