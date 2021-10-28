f85c6f1995c27318621fd77fe4e6cbfdb5582d82
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-27 22:47:04 -0400

    instruction generator accesses fwi

--- src/VLQRISC_Assembler/instructionGenerator.py
+++ src/VLQRISC_Assembler/instructionGenerator.py
@@ -1,5 +1,4 @@
 
-from src.Shared.utils import convert_int_bin_str
 import src.VLQRISC_Assembler.parser as parser
 import src.VLQRISC_Assembler.operations as operations
 
@@ -24,11 +23,11 @@ class InstructionGenerator():
             raise MissingOpcode("No opcode was parsed")
 
         if self.line_data.Rd_num:
-            self.Rd = convert_int_bin_str(self.line_data.Rd_num, 4)
+            self.Rd = self.line_data.Rd_num.bits
         if self.line_data.Rs1_num:
-            self.Rs1 = convert_int_bin_str(self.line_data.Rs1_num, 4)
+            self.Rs1 = self.line_data.Rs1_num.bits
         if self.line_data.Rs2_num:
-            self.Rs2 = convert_int_bin_str(self.line_data.Rs2_num, 4)
+            self.Rs2 = self.line_data.Rs2_num.bits
 
     def __generate_GPR_GPR_inst(self):
         return f"{self.opcode}{self.Rd}{self.Rs1}{self.Rs2}" + "0"*13
