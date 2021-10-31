47602e181ccb3f5e8e5cae4b56547196c4106a90
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-31 12:07:30 -0400

    Register and ALU

--- src/VLQRISC_Simulator/system.py
+++ src/VLQRISC_Simulator/system.py
@@ -1,13 +1,24 @@
+from __future__ import annotations
+from typing import Union
 from src.VLQRISC_Simulator.hw_definitions import MEMORY_SIZE, REGISTER_NAMES
 from src.Shared.fwi import FWI, FWI_unsigned
 
 
 class VLQRISC_System():
     class REGISTER():
-        def __init__(self, name: str):
-            self.name =
-            self.s = FWI(0, 8)
-            self.u = FWI_unsigned(0, 8)
+        def __init__(self, name: str, number: int):
+            self.name = name
+            self.NUMBER = REGISTER_NAMES[number]
+            self.s = FWI(0, 32)
+            self.u = FWI_unsigned(0, 32)
+
+        def set_unsigned(self, u_value: FWI_unsigned):
+            self.s = FWI.from_unsigned(u_value)
+            self.u = u_value
+
+        def set_signed(self, s_value: FWI):
+            self.s = s_value
+            self.u = FWI_unsigned.from_signed(s_value)
 
     class ALU():
         def __init__(self):
@@ -15,7 +26,7 @@ class VLQRISC_System():
             self.b = None  # input
             self.c = None
 
-        def set_operands(self, a: FWI, b: FWI):
+        def set_operands(self, a: Union[FWI, FWI_unsigned], b: Union[FWI, FWI_unsigned]):
             self.a = a  # input
             self.b = b  # input
 
@@ -23,12 +34,11 @@ class VLQRISC_System():
             self.set_operands(a, b)
             return a+b
 
-        def ADD_UNSIGNED(self, a: FWI_unsigned, b: FWI_unsigned) -> FWI:
+        def ADD_UNSIGNED(self, a: FWI_unsigned, b: FWI_unsigned) -> FWI_unsigned:
             self.set_operands(a, b)
             return a+b
 
-        @staticmethod
-        def AND(self, a: FWI_unsigned, b: FWI_unsigned) -> FWI:
+        def AND(self, a: FWI_unsigned, b: FWI_unsigned) -> FWI_unsigned:
             self.set_operands(a, b)
             return a+b
 
@@ -36,18 +46,16 @@ class VLQRISC_System():
         self.memory_table = []
         self.memory_table = [FWI_unsigned(0, 8)]*MEMORY_SIZE
 
-        self.register_table = []
-        for name in REGISTER_NAMES:
+        self.register_table: list[VLQRISC_System.REGISTER] = []
+        for i, name in enumerate(REGISTER_NAMES):
             self.register_table.append(
-                (name, FWI_unsigned(0, 32)))
+                self.REGISTER(name, i))
 
         self.alu = self.ALU()
 
     @ property
-    def register_table_bits(self):
-        register_table = []
-        for i, reg in enumerate(self.register_table):
-            register_table.append((reg[0], reg[1].bits))
-        return register_table
-
-    def register_table(self)
+    def register_table_bits(self) -> list[tuple[str, str]]:
+        register_table_bits: list[tuple[str, str]] = []
+        for reg in self.register_table:
+            register_table_bits.append((reg.name, reg.u.bits))
+        return register_table_bits
