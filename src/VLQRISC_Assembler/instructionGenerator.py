0123feaf438079fd469f54245400cab67ef22f92
Jamie Mortensen <16mortensenj@gmail.com>
2021-10-31 22:22:49 -0400

    added comparison branch to the instruction generator

--- src/VLQRISC_Assembler/instructionGenerator.py
+++ src/VLQRISC_Assembler/instructionGenerator.py
@@ -2,8 +2,8 @@
 import src.VLQRISC_Assembler.parser as parser
 import src.VLQRISC_Simulator.system as operations
 
-from src.Shared.fwi import FWI_unsigned
-from src.VLQRISC_Simulator.system import Instruction
+from src.Shared.fwi import FWI_unsigned, FWI
+from src.VLQRISC_Simulator.system import Instruction, OpTypes, Operations
 
 
 class MissingOpcode(Exception):
@@ -25,6 +25,8 @@ class InstructionGenerator():
             return self.__generate_GPR_GPR_inst()
         elif self.line_data.type == operations.OpTypes.NUM_GPR:
             return self.__generate_NUM_GPR_inst()
+        elif self.line_data.type == operations.OpTypes.COMP_BRANCH:
+            return self.__generate_COMP_BRANCH_inst()
         else:
             raise OpTypeNotRecognized("Instruction type not implemented")
 
@@ -42,6 +44,8 @@ class InstructionGenerator():
             self.Rs2 = self.line_data.Rs2_num.bits
         if self.line_data.immediate_operand:
             self.immediate_operand = self.line_data.immediate_operand.bits
+        if self.line_data.jump_address_str:
+            self.jump_address_str = self.line_data.jump_address_str
 
     def __generate_GPR_GPR_inst(self):
         return Instruction(FWI_unsigned.from_binary_str(f"{self.opcode}{self.Rd}{self.Rs1}{self.Rs2}" + "0"*15), operations.OpTypes.GPR_GPR)
@@ -49,3 +53,22 @@ class InstructionGenerator():
     def __generate_NUM_GPR_inst(self):
 
         return Instruction(FWI_unsigned.from_binary_str(f"{self.opcode}{self.Rd}{self.Rs1}{self.immediate_operand}"), operations.OpTypes.NUM_GPR)
+
+    def __generate_COMP_BRANCH_inst(self):
+        jump_address: FWI_unsigned
+        if self.jump_address_str[0:2] == "0b":
+            if self.jump_address_str:
+                jump_address = FWI_unsigned.address_from_binary_str(
+                    self.jump_address_str[2:])
+        elif self.jump_address_str.startswith("0x"):
+            pass
+            raise NotImplementedError("Hexadecimal input is not implemented")
+            # assume
+        else:
+            #decimal or label
+            if self.jump_address_str.isnumeric():
+                jump_address = FWI_unsigned(int(self.jump_address_str), 16)
+            else:
+                raise NotImplementedError("Labels input is not implemented")
+
+        return Instruction(FWI_unsigned.from_binary_str(f"{self.opcode}{self.Rs1}{self.Rs2}000{jump_address.bits}"), operations.OpTypes.COMP_BRANCH)
